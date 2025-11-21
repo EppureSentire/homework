c86f6587d9fdc7f5807f26a0c1073746f7425d51
EppureSentire <19021947041@163.com>
2025-11-21 16:40:23 +0800

    Update 1_text.c

--- 1_text.c
+++ 1_text.c
@@ -6,32 +6,12 @@
 
 int main()
 {
-	int shuju[3][3];
-	int i;
-	int j;
-	for (i = 0; i < 3; i++)
-	{
-		for (j = 0;j < 3;j++)
-		{
-			printf("请输入%d行第%d列的元素：",i+1,j+1);
-			scanf("%d", &shuju[i][j]);
-		}
-	}
-	printf("输入的矩阵为：\n");
-	for (i = 0; i < 3; i++)
-	{
-		for (j = 0; j < 3; j++)
-		{
-			if ((i == 0 && j == 0 )||(i == 1 && j == 0)||(i == 2 && j == 0))
-			{
-				printf("%d",shuju[i][j]);
-			}
-			else
-			{
-				printf(" %d",shuju[i][j]);
-			}
-		}
-		printf("\n");
-	}
+	int a;
+	printf("请输入a的数值：");
+	scanf("%d", &a);
+	int* p = &a;
+	*p = *p + 10;
+	printf("%d ", a);
+	printf("%d", *p);
 	return 0;
 }
